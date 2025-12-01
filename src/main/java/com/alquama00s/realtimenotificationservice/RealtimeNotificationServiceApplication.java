6fea38d96b22b9faf791bf4b11c38748acdf944b
Alquama <alquamasalim000@gmail.com>
2025-12-01 12:20:47 +0530

    init

---
+++ src/main/java/com/alquama00s/realtimenotificationservice/RealtimeNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alquama00s.realtimenotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alquama00s/realtimenotificationservice/RealtimeNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquama00s.realtimenotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
